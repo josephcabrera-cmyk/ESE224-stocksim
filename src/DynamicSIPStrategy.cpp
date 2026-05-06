dfee86399337d0307e593347145e197af8ca42e8
Danny Ouyang <dannyou083@gmail.com>
2026-05-06 14:35:45 -0400

    c

--- src/DynamicSIPStrategy.cpp
+++ src/DynamicSIPStrategy.cpp
@@ -14,14 +14,25 @@ double monthlyCapital, int startYear, int endYear) {
 		return result;
 	}
 
+	int totalMonths = (endYear - startYear) * 12; 
+	double totalBudget = totalMonths * monthlyCapital;
+	double remainingBudget = totalBudget;
+
 	double totalInvested = 0.0;
 	double shares = 0.0;
+
 	vector<double> portfolioValues;
 	vector<double> recentPrices;
 
-	for (int year = startYear; year <= endYear; year++) {
-        
+	int monthsCompleted = 0;
+
+	for (int year = startYear; year < endYear; year++) {
+
 		for (int month = 1; month <= 12; month++) {
+
+			if (monthsCompleted >= totalMonths) {
+				break;
+			}
 
 			PriceNode* current = history->getHead();
 
@@ -32,38 +43,67 @@ double monthlyCapital, int startYear, int endYear) {
 				if (nodeYear == year && nodeMonth == month) {
 					double closePrice = current->close;
 
-					double spend = monthlyCapital;
+					int remainingMonths = totalMonths - monthsCompleted;
+					double baseSpend = remainingBudget / remainingMonths;
+					double spend = baseSpend;
+
 					if (recentPrices.size() >= 12) {
 						double high = recentPrices[0];
 						double low = recentPrices[0];
+
 						for (size_t i = 1; i < recentPrices.size(); i++) {
 							if (recentPrices[i] > high) {
 								high = recentPrices[i];
 							}
+
 							if (recentPrices[i] < low) {
 								low = recentPrices[i];
 							}
 						}
-						double dipFromHigh = (high - closePrice) / high * 100;
-						double rallyFromLow = (closePrice - low) / low * 100;
+
+						double dipFromHigh = 0.0;
+						double rallyFromLow = 0.0;
+
+						if (high > 0) {
+							dipFromHigh = (high - closePrice) / high * 100.0;
+						}
+
+						if (low > 0) {
+							rallyFromLow = (closePrice - low) / low * 100.0;
+						}
 
 						if (dipFromHigh >= dipThreshold) {
-							spend = multiplier * monthlyCapital;
+							spend = multiplier * baseSpend;
 						}
 						else if (rallyFromLow >= rallyThreshold) {
-							spend = 0.5 * monthlyCapital;
+							spend = 0.5 * baseSpend;
 						}
 					}
 
-					double totalBudget = (endYear - startYear + 1) * 12 * monthlyCapital;
-					if (totalInvested + spend > totalBudget) {
-						spend = totalBudget - totalInvested;
+					double minSpend = 0.25 * baseSpend;
+					double maxSpend = 3.0 * baseSpend;
+
+					if (spend < minSpend) {
+						spend = minSpend;
+					}
+
+					if (spend > maxSpend) {
+						spend = maxSpend;
+					}
+
+					if (spend > remainingBudget) {
+						spend = remainingBudget;
+					}
+
+					if (monthsCompleted == totalMonths - 1) {
+						spend = remainingBudget;
 					}
 
 					if (spend > 0) {
-						double sharesBought = spend /closePrice;
+						double sharesBought = spend / closePrice;
 						shares += sharesBought;
 						totalInvested += spend;
+						remainingBudget -= spend;
 
 						double portfolioValue = shares * closePrice;
 
@@ -74,9 +114,12 @@ double monthlyCapital, int startYear, int endYear) {
 					}
 
 					recentPrices.push_back(closePrice);
+
 					if (recentPrices.size() > 12) {
 						recentPrices.erase(recentPrices.begin());
 					}
+
+					monthsCompleted++;
 
 					break;
 				}
@@ -86,15 +129,14 @@ double monthlyCapital, int startYear, int endYear) {
 		}
 	}
 
-	if (!portfolioValues.empty()) {
+	if (!portfolioValues.empty() && result.totalInvested > 0) {
 		result.maxDrawdown = calculateMaxDrawdown(portfolioValues);
 		result.totalReturn = (result.finalValue - result.totalInvested) / result.totalInvested * 100.0;
-		result.cagr = calculateCAGR(portfolioValues.front(), portfolioValues.back(), endYear - startYear + 1);
+		result.cagr = calculateCAGR(portfolioValues.front(), portfolioValues.back(), endYear - startYear);
 	}
 
 	return result;
 }
-
 string DynamicSIPStrategy::getName() const { 
 	return "Dynamic SIP";
 }
